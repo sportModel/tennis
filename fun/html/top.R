53bf8f1fda741f6c2ba634d85daa4b89ba907ea7
Patrick Breheny <pat5547@gmail.com>
2016-04-05 06:14:30 -0500

    Testing table.html

--- fun/html/top.R
+++ fun/html/top.R
@@ -1,4 +1,8 @@
 top <- function(psm, name) {
-  Tab <- cbind(psm$Delta + psm$Alpha, Overall=psm$Delta)
-  print(htmlTable(Tab), file="")
+  Tab <- as.data.frame(psm$Delta + psm$Alpha)
+  m <- ncol(psm$Eta)
+  Tab$Last12mo <- psm$Delta - psm$Eta[,m-12]
+  Tab$Last3mo <- psm$Delta - psm$Eta[,m-3]
+  Tab$Overall <- psm$Delta
+  print(htmlTable(Tab, class="ctable"), name=name)
 }
