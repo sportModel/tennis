2eca62d5c95f30a3d5cf91e479f22d0398c51004
Patrick Breheny <pat5547@gmail.com>
2016-04-15 12:32:39 -0500

    Monte Carlo 2016

--- tournaments/montecarlo2106.R
+++ tournaments/montecarlo2106.R
@@ -1,12 +1,21 @@
 require(html)
 load("psm/atp.RData")
+
+# Start of tournament
 res <- urlTour("http://www.atpworldtour.com/en/scores/current/monte-carlo/410/draws",
                "Clay", sets=3)
 tourTable(res, "_posts/tab/montecarlo2016a")
 
+# R16
 res <- urlTour("http://www.atpworldtour.com/en/scores/current/monte-carlo/410/draws",
                "Clay", sets=3, roundID="V3")
 tourTable(res, "_posts/tab/montecarlo2016b")
 
+# SF
+res <- urlTour("http://www.atpworldtour.com/en/scores/current/monte-carlo/410/draws",
+               "Clay", sets=3, roundID="V5")
+tourTable(res, "_posts/tab/montecarlo2016c")
+
 # Individual matches
 predictMatch("Djokovic N.", "Vesely J.", "Clay", 3, verbose=TRUE)
+predictMatch("Tsonga J.W.", "Federer R.", "Clay", 3, verbose=TRUE)
